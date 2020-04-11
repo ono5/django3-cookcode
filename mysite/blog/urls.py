28b149b03982f9757a0fce8bd5c29e5b9c890a92
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-04-12 07:24:08 +0900

    Added url to post and implements get_absolute_url

---
+++ mysite/blog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+# application namespace
+app_name = 'blog'
+
+# https://docs.djangoproject.com/en/3.0/topics/http/urls/#path-converters
+urlpatterns = [
+    # post views
+    path('', views.post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+         views.post_detail,
+         name='post_detail'),
+]
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path, include
 from django.contrib import admin
-from django.urls import path
 
+# https://docs.djangoproject.com/en/3.0/topics/http/urls/#url-namespaces
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls', namespace='blog')),
 ]
