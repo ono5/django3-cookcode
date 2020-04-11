9e7776544c2ed7d999d4c64994ce4f7596600254
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-04-11 19:06:00 +0900

    Create DB Manager

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,6 +3,11 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -23,8 +28,13 @@ class Post(models.Model):
                               choices=STATUS_CHOICES,
                               default='draft')
 
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
+
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
         return self.title
+
+
