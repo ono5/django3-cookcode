b4dee0f5daaaa42249a7edad7019101a1c662cf1
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-04-12 13:23:28 +0900

    Added Class-base view

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -7,7 +7,8 @@ app_name = 'blog'
 # https://docs.djangoproject.com/en/3.0/topics/http/urls/#path-converters
 urlpatterns = [
     # post views
-    path('', views.post_list, name='post_list'),
+    # path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.post_detail,
          name='post_detail'),
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 from .models import Post
 
 
@@ -30,3 +31,10 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts' # default is object_list
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
