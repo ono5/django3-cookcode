fc62e8d188c6004a880804b4107506f9cad7846f
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-04-20 16:26:41 +0900

    Added media settings

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/' # is the bae URL that serves media files uploaded by users.
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/') # is the local path where these files reside, which you build by dynamically prepending the BASE_DIR variable.
